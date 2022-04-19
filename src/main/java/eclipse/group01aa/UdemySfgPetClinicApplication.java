d9cdc4ceec27d765ef0037fce4c5e2fb6fb81091
tgmcondon <103388074+tgmcondon@users.noreply.github.com>
2022-04-18 22:23:47 -0500

    initial

---
+++ src/main/java/eclipse/group01aa/UdemySfgPetClinicApplication.java
@@ -0,0 +1,13 @@
+package eclipse.group01aa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UdemySfgPetClinicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UdemySfgPetClinicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/eclipse/group01aa/UdemySfgPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package eclipse.group01aa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UdemySfgPetClinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
